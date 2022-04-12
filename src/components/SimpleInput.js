836bacdbaa60ccb3d5124dcacb35330efc82512a
Khurshida-Meem <meemrumi59@gmail.com>
2022-04-12 13:15:16 +0600

    added small validation

--- src/components/SimpleInput.js
+++ src/components/SimpleInput.js
@@ -1,19 +1,55 @@
-import { useState } from "react";
+import { useState,useRef,useEffect } from "react";
 
 const SimpleInput = (props) => {
 
+  const nameInputRef = useRef()
   const [enteredName, setEnteredName] = useState('');
+  const [error, setError] = useState(false);
+  const [reEnterValue, setReEnterValue] = useState(false);
+
+  useEffect(() => {
+    if (error) {
+      console.log('valid input');
+    }
+  }, [error]);
 
   const inputChangeHandler = e => {
-    e.preventDefault();
     setEnteredName(e.target.value)
   }
 
+  const handleSubmit = e => {
+    e.preventDefault();
+
+    setReEnterValue(true);
+
+    if (enteredName.trim() === '') {
+      setError(false)
+      return;
+    }
+
+    setError(true);
+
+    setEnteredName('');
+
+  }
+
+  const nameInputIsValid = !error && reEnterValue;
+
+  const nameinputClasses = nameInputIsValid ? 'form-control invalid' : 'form-control';
+
   return (
-    <form>
-      <div className='form-control'>
+    <form onSubmit={handleSubmit}>
+      <div className={nameinputClasses}>
         <label htmlFor='name'>Your Name</label>
-        <input type='text' id='name' />
+        <input
+          ref={nameInputRef}
+          type='text'
+          id='name'
+          onChange={inputChangeHandler}
+          value={enteredName}
+
+        />
+        {nameInputIsValid && <p className="error-text">Please enter a valid data</p>}
       </div>
       <div className="form-actions">
         <button>Submit</button>
